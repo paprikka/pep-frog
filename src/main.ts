09776d014b9a07ee468b1ddbdce8815a37642e9e
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-10-19 16:25:27 +0300

    feat: add persistence

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,50 @@ $$<SVGFEDisplacementMapElement>('feDisplacementMap').forEach((node) =>
 const textContainer = $<SVGTextElement>('.frog__blessing')!
 const { floor, random } = Math
 
-const pickRandomItem = (phrases: string[]): string =>
-    phrases[floor(phrases.length * random())]
-
-const render = () =>
-    (textContainer.textContent = dict.map(pickRandomItem).join(' '))
+const render = (phrase: string) => (textContainer.textContent = phrase)
 
 const cta = $<HTMLButtonElement>('.cta')!
 
-cta.onclick = render
+const getNewBlessing = () => {
+    const indices = dict.map((phrases) => floor(phrases.length * random()))
+    location.hash = `#${indices.join(',')}`
+    render(indices.map((phraseInd, ind) => dict[ind][phraseInd]).join(' '))
+}
+cta.onclick = getNewBlessing
 
-render()
+const shareBtn = $<HTMLButtonElement>('.share')!
+
+const wait = (time: number) => () =>
+    new Promise((resolve) => setTimeout(resolve, time))
+
+const toastEl = $<HTMLDivElement>('.toast')!
+
+shareBtn.onclick = () => {
+    const text = location.href
+    const type = 'text/plain'
+    const blob = new Blob([text], { type })
+    const data = [new (ClipboardItem as any)({ [type]: blob })]
+
+    navigator.clipboard.write(data).then(function () {
+        Promise.resolve()
+            .then(() => toastEl.classList.add('toast--visible'))
+            .then(wait(2000))
+            .then(() => toastEl.classList.remove('toast--visible'))
+
+        // window.alert('URL copied to clipboard!')
+    })
+}
+
+new Promise<string>((resolve, reject) => {
+    const maybeIndices = location.hash
+        .slice(1)
+        .split(',')
+        .map((_) => parseInt(_, 10))
+        .map((phraseInd, ind) => dict[ind][phraseInd])
+        .filter(Boolean)
+
+    if (maybeIndices.length !== 4) return reject()
+    resolve(maybeIndices.join(' '))
+})
+    .then(render)
+    .catch(getNewBlessing)
